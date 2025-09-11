8eda7b0087cf020dc11d473c3fba28decaed3172
mzl980425 <1198079622@qq.com>
2025-09-11 16:47:01 +0800

    feat(env): add isProd, isCloudflare and print env

--- utils/env.ts
+++ utils/env.ts
@@ -1,10 +1,15 @@
 export const isDev = Deno.env.get("NODE_ENV") === "development";
 
+export const isProd = Deno.env.get("NODE_ENV") === "production";
+
+export const isCloudflare = !!Deno.env.get("CLOUDFLARE_ACCOUNT_ID");
+
 export const isDenoDeploy = !!Deno.env.get("DENO_DEPLOYMENT_ID");
 
 export const isDenoDeployEA = !!Deno.env.get("DENO_DEPLOY");
 
-console.log('================================= Environment Variables =================================')
+console.log("================================= Environment Variables =================================");
 Object.entries(Deno.env.toObject()).forEach(([key, value]) => {
   console.log(`${key}=${value}`);
 });
+console.table({ isCloudflare, isDenoDeploy, isDenoDeployEA, isDev, isProd });
